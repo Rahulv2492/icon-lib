d25f98fbc11efde8d281bf1a0d0e4cb16ab002e9
irahulv <rahulv2492@gmail.com>
2023-10-31 21:07:18 +0530

    initial commit

---
+++ rollup.config.mjs
@@ -0,0 +1,43 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import json from "@rollup/plugin-json";
+
+import packageJson from "./package.json" assert { type: "json" };
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    external: [
+      "react-native-web",
+      "react-native",
+      "react-native/Libraries/Utilities/codegenNativeComponent.js",
+    ],
+    plugins: [
+      peerDepsExternal(),
+      json(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+    ],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
+];
